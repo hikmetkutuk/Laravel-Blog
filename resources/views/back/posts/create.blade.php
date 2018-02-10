40af5a69d05d8f9a0c538aa41672e19be5f44c55
hikmetkutuk <hikmetkutuk@gmail.com>
2018-02-10 16:08:09 +0300

    created post view

---
+++ resources/views/back/posts/create.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hikmetis
+ * Date: 2/10/18
+ * Time: 4:00 PM
+ */
+?>
+
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-center">Create a new post</h2>
+    @stop
