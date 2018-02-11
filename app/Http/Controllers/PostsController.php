b3c12bffa2def4feb24c07a04f29ce570182e01e
hikmetkutuk <hikmetkutuk@gmail.com>
2018-02-11 03:17:20 +0300

    data validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -35,6 +35,13 @@ class PostsController extends Controller
     public function store(Request $request)
     {
         //dd($request->all());
+        $this->validate($request, [
+            'title' => 'required',
+            'featured' => 'required|image',
+            'content' => 'required'
+        ]);
+
+        dd($request->all());
     }
 
     /**
