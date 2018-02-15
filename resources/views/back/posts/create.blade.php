899943604385af05838053097ef7c893420df610
hikmetkutuk <hikmetkutuk@gmail.com>
2018-02-15 23:51:34 +0300

    added select box for category

--- resources/views/back/posts/create.blade.php
+++ resources/views/back/posts/create.blade.php
@@ -36,6 +36,14 @@
                     <input type="file" name="featured" class="form-control">
                 </div>
                 <div class="form-group">
+                  <label for="category">Select Category</label>
+                  <select name="cat_id" id="category" class="form-control">
+                    @foreach($categories as $category)
+                      <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                  </select>
+                </div>
+                <div class="form-group">
                     <label for="content">Content</label>
                     <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
                 </div>
