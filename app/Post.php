c4423750b2a1feef75182874790b315b89fba834
hikmetkutuk <hikmetkutuk@gmail.com>
2018-02-19 01:49:29 +0300

    creating tags model

--- app/Post.php
+++ app/Post.php
@@ -31,4 +31,9 @@ class Post extends Model
     {
         return $this->belongsTo('App\Category');
     }
+
+    public function tags()
+    {
+      return $this->belongsToMany('App\Tag');
+    }
 }
