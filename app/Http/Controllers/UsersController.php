7180935be7c9bfd2db9d9e62ceb1739a3b56e762
Hikmet Kütük <hikmetkutuk@gmail.com>
2018-04-25 20:53:58 +0300

    user permissions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -50,7 +50,8 @@ class UsersController extends Controller
         ]);
 
         $profile = Profile::create([
-            'user_id' => $user->id
+            'user_id' => $user->id,
+            'avatar' => '/uploads/avatars/user.png'
         ]);
 
         Session::flash('success', 'User added succesfully');
@@ -98,6 +99,30 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    public function admin($id)
+    {
+        $user = User::find($id);
+        $user->admin = 1;
+
+        $user->save();
+
+        Session::flash('success', 'Succesfully changed user permissions');
+
+        return redirect()->back();
+    }
+
+    public function not_admin($id)
+    {
+        $user = User::find($id);
+        $user->admin = 0;
+
+        $user->save();
+
+        Session::flash('success', 'Succesfully changed user permissions');
+
+        return redirect()->back();
+    }
+
     public function destroy($id)
     {
         //
