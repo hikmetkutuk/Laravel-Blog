af89ce1090ee85fea15a1781622dfbc7d2519354
Hikmet Kütük <hikmetkutuk@gmail.com>
2018-05-10 05:04:21 +0300

    updating user profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Session;
 
 class ProfilesController extends Controller
 {
@@ -69,7 +70,44 @@ class ProfilesController extends Controller
      */
     public function update(Request $request)
     {
+        $this->validate($request, [
+           'name' => 'required',
+           'email' => 'required',
+           'instagram' => 'required|url',
+           'youtube' => 'required|url'
+        ]);
 
+        $user = Auth::user();
+
+        if($request->hasFile('avatar'))
+        {
+            $avatar = $request->avatar;
+            $avatar_new_name = time() . $avatar->getClientOriginalName();
+
+            $avatar->move('uploads/avatars', $avatar_new_name);
+
+            $user->profile->avatar = 'uploads/avatars/' . $avatar_new_name;
+
+            $user->profile->save();
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->profile->instagram = $request->instagram;
+        $user->profile->youtube = $request->youtube;
+
+        $user->save();
+        $user->profile->save();
+
+        if($request->has('password'))
+        {
+            $user->password = bcrypt($request->password);
+            $user->save();
+        }
+
+        Session::flash('success', 'Profile updated');
+
+        return redirect()->back();
     }
 
     /**
--- resources/views/back/users/profile.blade.php
+++ resources/views/back/users/profile.blade.php
@@ -10,7 +10,7 @@
         </div>
 
         <div class="panel-body">
-            <form action="{{ route('user.profile.update') }}" method="post">
+            <form action="{{ route('user.profile.update') }}" method="post" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label for="name">Username</label>
