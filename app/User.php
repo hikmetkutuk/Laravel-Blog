6bc86d44e219f2dfe2fde25192912bca4ea523d3
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-02-21 22:19:37 +0300

    update syntax in User.php

--- app/User.php
+++ app/User.php
@@ -5,28 +5,18 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
+    
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password', 'admin'
     ];
 
-    public function profile()
-    {
+    public function profile() {
         return $this->hasOne('App\Profile');
     }
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    
     protected $hidden = [
         'password', 'remember_token',
     ];
