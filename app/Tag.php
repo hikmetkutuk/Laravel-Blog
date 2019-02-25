831bcd6eb4a61b47df306d110a6de7f18ad6024f
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-02-25 19:39:21 +0300

    update Tag.php

--- app/Tag.php
+++ app/Tag.php
@@ -4,12 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
-{
+class Tag extends Model {
     protected $fillable = ['tag'];
 
-    public function posts()
-    {
+    public function posts() {
         return $this->belongsToMany('App\Post');
     }
 }
