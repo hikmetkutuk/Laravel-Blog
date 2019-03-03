e162f9ba3a84e8bbb6253df5030b8be2784eb8e9
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-03-03 22:22:26 +0300

    update TagsController.php

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -7,36 +7,17 @@ use App\Tag;
 use Illuminate\Support\Facades\Session;
 use Sessions;
 
-class TagsController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+class TagsController extends Controller {
+   
+    public function index() {
         return view('back.tags.index')->with('tags', Tag::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+    public function create() {
         return view('back.tags.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
+    
+    public function store(Request $request) {
         $this->validate($request, [
            'tag' => 'required'
         ]);
@@ -50,39 +31,13 @@ class TagsController extends Controller
         return redirect()->route('tags');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
+    public function edit($id) {
         $tag = Tag::find($id);
 
         return view('back.tags.edit')->with('tag', $tag);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         $this->validate($request, [
            'tag' => 'required'
         ]);
@@ -97,14 +52,7 @@ class TagsController extends Controller
         return redirect()->route('tags');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         Tag::destroy($id);
 
         Session::flash('success', 'Tag deleted successfully');
