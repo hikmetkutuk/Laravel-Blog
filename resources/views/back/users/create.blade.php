76578697db95e076c94271dd2697151f2f5924ef
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-03-03 23:54:55 +0300

    update create.blade.php

--- resources/views/back/users/create.blade.php
+++ resources/views/back/users/create.blade.php
@@ -13,7 +13,7 @@
             <form action="{{ route('user.store') }}" method="post">
                 {{ csrf_field() }}
                 <div class="form-group">
-                    <label for="name">User</label>
+                    <label for="name">Name</label>
                     <input type="text" name="name" class="form-control">
                 </div>
                 <div class="form-group">
