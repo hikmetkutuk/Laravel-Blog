d530584e178e12df6ecb589090304be5d968940b
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-03-03 23:23:40 +0300

    update Profile.php

--- app/Profile.php
+++ app/Profile.php
@@ -4,10 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
-{
-    public function user()
-    {
+class Profile extends Model {
+
+    public function user() {
         return $this->belongsTo('App\User');
     }
 
