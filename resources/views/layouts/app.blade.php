87dca35d346794364ca939dae789b1d286f65837
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-03-04 17:40:50 +0300

    add WYSIWYG editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" 
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    @yield('styles')
 </head>
 <body>
     <div id="app">
@@ -132,5 +133,6 @@
         toastr.info("{{ Session::get('info') }}")
       @endif
     </script>
+    @yield('scripts')
 </body>
 </html>
