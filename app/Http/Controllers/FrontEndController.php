27146f92a314f6fd440e2c6c579fce990c288f1e
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-03-07 16:27:37 +0300

    get latest posts

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Setting;
 use App\Category;
+use App\Post;
 
 class FrontEndController extends Controller {
     
     public function index() {
         return view('index')
             ->with('title', Setting::first()->site_name)
-            ->with('categories', Category::all());
+            ->with('categories', Category::all())   //Category::take(5)->get()
+            ->with('latest', Post::orderBy('created_at', 'desc')->take(3)->get()); 
     }
 }
