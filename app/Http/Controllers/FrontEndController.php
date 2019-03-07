a484cc46d3397a437b7501273f7ea975438f3886
Hikmet Kütük <hikmetkutuk@gmail.com>
2019-03-07 15:23:21 +0300

    get categories to front page

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Setting;
+use App\Category;
 
 class FrontEndController extends Controller {
     
     public function index() {
-        return view('index')->with('title', Setting::first()->site_name);
+        return view('index')
+            ->with('title', Setting::first()->site_name)
+            ->with('categories', Category::all());
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,15 +8,15 @@
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300, 400,700|Inconsolata:400,700" rel="stylesheet">
 
     <link rel="stylesheet" href={{ asset("_front/css/bootstrap.css") }}>
-    <link rel="stylesheet" href={{ asset("css/animate.css") }}>
-    <link rel="stylesheet" href={{ asset("css/owl.carousel.min.css") }}>
+    <link rel="stylesheet" href={{ asset("_front/css/animate.css") }}>
+    <link rel="stylesheet" href={{ asset("_front/css/owl.carousel.min.css") }}>
 
-    <link rel="stylesheet" href={{ asset("fonts/ionicons/css/ionicons.min.css") }}>
-    <link rel="stylesheet" href={{ asset("fonts/fontawesome/css/font-awesome.min.css") }}>
-    <link rel="stylesheet" href={{ asset("fonts/flaticon/font/flaticon.css") }}>
+    <link rel="stylesheet" href={{ asset("_front/fonts/ionicons/css/ionicons.min.css") }}>
+    <link rel="stylesheet" href={{ asset("_front/fonts/fontawesome/css/font-awesome.min.css") }}>
+    <link rel="stylesheet" href={{ asset("_front/fonts/flaticon/font/flaticon.css") }}>
 
     <!-- Theme Style -->
-    <link rel="stylesheet" href={{ asset("css/style.css") }}>
+    <link rel="stylesheet" href={{ asset("_front/css/style.css") }}>
   </head>
   <body>
     
@@ -443,15 +443,15 @@
     <!-- loader -->
     <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
 
-    <script src={{ asset("js/jquery-3.2.1.min.js") }}></script>
-    <script src={{ asset("js/jquery-migrate-3.0.0.js") }}></script>
-    <script src={{ asset("js/popper.min.js") }}></script>
-    <script src={{ asset("js/bootstrap.min.js") }}></script>
-    <script src={{ asset("js/owl.carousel.min.js") }}></script>
-    <script src={{ asset("js/jquery.waypoints.min.js") }}></script>
-    <script src={{ asset("js/jquery.stellar.min.js") }}></script>
+    <script src={{ asset("_front/js/jquery-3.2.1.min.js") }}></script>
+    <script src={{ asset("_front/js/jquery-migrate-3.0.0.js") }}></script>
+    <script src={{ asset("_front/js/popper.min.js") }}></script>
+    <script src={{ asset("_front/js/bootstrap.min.js") }}></script>
+    <script src={{ asset("_front/js/owl.carousel.min.js") }}></script>
+    <script src={{ asset("_front/js/jquery.waypoints.min.js") }}></script>
+    <script src={{ asset("_front/js/jquery.stellar.min.js") }}></script>
 
     
-    <script src={{ asset("js/main.js") }}></script>
+    <script src={{ asset("_front/js/main.js") }}></script>
   </body>
 </html>
